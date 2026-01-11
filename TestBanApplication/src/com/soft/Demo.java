e2accf1ff16e8aa5c5bbbc6788a9f554baaea87b
irisesoftware <info@irisesoft.in>
2026-01-11 10:07:56 +0530

    changes in demo ava file

--- TestBanApplication/src/com/soft/Demo.java
+++ TestBanApplication/src/com/soft/Demo.java
@@ -3,5 +3,6 @@ package com.soft;
 public class Demo {
 public static void main(String[] args) {
 	System.out.println("Welcome to java programming...");
+	System.out.println("Welcome to github...");
 }
 }
