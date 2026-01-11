bf87fbaf41ecfffad3d0c1f590aea343728a3177
Irise <info@irisesoft.in>
2026-01-11 10:09:52 +0530

    Update Demo.java

--- TestBanApplication/src/com/soft/Demo.java
+++ TestBanApplication/src/com/soft/Demo.java
@@ -4,5 +4,6 @@ public class Demo {
 public static void main(String[] args) {
 	System.out.println("Welcome to java programming...");
 	System.out.println("Welcome to github...");
+	System.out.println("Welcome to Irise software...");
 }
 }
